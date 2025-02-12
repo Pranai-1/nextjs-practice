6c5c35eedc24f22053229de778fea107b6bce9e2
Pranai-1 <reddypranai2017@gmail.com>
2025-02-12 15:47:41 +0530

    group routing

---
+++ app/(group1)/dashboard/page.tsx
@@ -0,0 +1,5 @@
+export default async function Dashboard(){
+    return(
+        <p>I am in dashboard page</p>
+    )
+}
---
+++ app/(group2)/settings/page.tsx
@@ -0,0 +1,5 @@
+export default async function Settings(){
+    return(
+        <p>I am in settings page</p>
+    )
+}
