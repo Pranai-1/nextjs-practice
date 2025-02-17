369a13c7dc5c7174e8e566295aa3e3fa90bf0be9
Pranai-1 <reddypranai2017@gmail.com>
2025-02-17 16:10:20 +0530

    Incremental static regeneration

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
+"use client";
+import { useState } from "react";
 
 
 export default function Home() {
+  const[state,setState]=useState(0)
   return (
-    <div className="text-black">Welcome</div>
+    <div className="text-black">
+      <p>Hello there</p>
+      <p>I am in main page {state}</p>
+      <button onClick={()=>setState((prev)=>prev+1)}>Press here</button>
+    </div>
+  
   );
 }
